d2df67b7205a1546a717fce4ccfc5b2231b659b3
Sallow <t.mavin.t@gmail.com>
2025-01-25 12:57:24 +0000

    Bubble Floating Working

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,18 @@
 using System.Collections.Generic;
+using System.Collections;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
     //Floats around on screen, ignore Y movement, screen tricking
 
-    //public List<Transform> bubbleVertices = new List<Transform>();
+    public List<Transform> bubbleVertices = new List<Transform>();
     public Transform bubbleCentre;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StartCoroutine(KeepBubbleAtCentre());
     }
 
     // Update is called once per frame
@@ -22,10 +23,45 @@ public class Player : MonoBehaviour
 
     void HandleMovement()
     {
-        if (Input.GetKeyDown(KeyCode.D))
+        if (Input.GetKey(KeyCode.D))
         {
-            bubbleCentre.GetComponent<Rigidbody2D>().AddForce(new Vector2(1,0));
+            //bubbleCentre.GetComponent<Rigidbody2D>().AddForce(new Vector2(10,0));
+            foreach (Transform vertex in bubbleVertices)
+            {
+                if (UnityEngine.Random.Range(0,120) == 1)
+                {
+                    vertex.GetComponent<Rigidbody2D>().AddForce(new Vector2(20,0));
+                    //vertex.position = new Vector2(vertex.position.x + 1, vertex.position.y);
+                }
+            }
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+           //bubbleCentre.GetComponent<Rigidbody2D>().AddForce(new Vector2(-10,0));
+           foreach (Transform vertex in bubbleVertices)
+            {
+                if (UnityEngine.Random.Range(0,120) == 1)
+                {
+                    vertex.GetComponent<Rigidbody2D>().AddForce(new Vector2(-20,0));
+                    //vertex.position = new Vector2(vertex.position.x - 1, vertex.position.y);
+                }
+            }
         }
     }
 
+    IEnumerator KeepBubbleAtCentre()
+    {
+        while (true)
+        {
+            if (bubbleCentre.position.y > 0)
+            {
+                bubbleCentre.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,-0.75f));
+            }
+            else if (bubbleCentre.position.y < 0)
+            {
+                bubbleCentre.GetComponent<Rigidbody2D>().AddForce(new Vector2(0,0.75f));
+            }
+            yield return new WaitForSeconds(0.01f);
+        } 
+    }
 }
